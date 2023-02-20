b4b6b265b86cebf6e59ca3bc99a2c425c7ddd506
marcos2paulinojr <124791219+marcos2paulinojr@users.noreply.github.com>
2023-02-20 17:46:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,12 @@ function updateClock() {
   var hours = now.getHours();
   var minutes = now.getMinutes();
   var seconds = now.getSeconds();
-
   var timeString = `${addZeroBefore(hours)}:${addZeroBefore(minutes)}:${addZeroBefore(seconds)}`;
   var clock = document.getElementById("clock");
   clock.textContent = timeString;
 }
-
 function addZeroBefore(num) {
   return (num < 10 ? "0" : "") + num;
 }
-
 updateClock();
 setInterval(updateClock, 1000);
